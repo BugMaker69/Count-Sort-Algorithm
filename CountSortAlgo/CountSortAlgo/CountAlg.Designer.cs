bc02f95e759912b661329b45d46a839cd2c06b25
Omar Elsaadany <115019564+BugMaker69@users.noreply.github.com>
2022-12-06 20:11:40 +0200

    Add files via upload

---
+++ CountSortAlgo/CountSortAlgo/CountAlg.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace CountSortAlgo
+{
+    partial class CountAlg
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EnteredData = new System.Windows.Forms.TextBox();
+            this.AddData = new System.Windows.Forms.Button();
+            this.DeleteData = new System.Windows.Forms.Button();
+            this.SortData = new System.Windows.Forms.Button();
+            this.ClearData = new System.Windows.Forms.Button();
+            this.ListData = new System.Windows.Forms.ListBox();
+            this.OutPut = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // EnteredData
+            // 
+            this.EnteredData.Location = new System.Drawing.Point(12, 21);
+            this.EnteredData.Name = "EnteredData";
+            this.EnteredData.Size = new System.Drawing.Size(100, 20);
+            this.EnteredData.TabIndex = 0;
+            // 
+            // AddData
+            // 
+            this.AddData.Location = new System.Drawing.Point(12, 62);
+            this.AddData.Name = "AddData";
+            this.AddData.Size = new System.Drawing.Size(75, 23);
+            this.AddData.TabIndex = 1;
+            this.AddData.Text = "Add";
+            this.AddData.UseVisualStyleBackColor = true;
+            this.AddData.Click += new System.EventHandler(this.AddData_Click);
+            // 
+            // DeleteData
+            // 
+            this.DeleteData.Location = new System.Drawing.Point(12, 196);
+            this.DeleteData.Name = "DeleteData";
+            this.DeleteData.Size = new System.Drawing.Size(75, 23);
+            this.DeleteData.TabIndex = 2;
+            this.DeleteData.Text = "Delete";
+            this.DeleteData.UseVisualStyleBackColor = true;
+            this.DeleteData.Click += new System.EventHandler(this.DeleteData_Click_1);
+            // 
+            // SortData
+            // 
+            this.SortData.Location = new System.Drawing.Point(12, 107);
+            this.SortData.Name = "SortData";
+            this.SortData.Size = new System.Drawing.Size(75, 23);
+            this.SortData.TabIndex = 3;
+            this.SortData.Text = "Sort";
+            this.SortData.UseVisualStyleBackColor = true;
+            this.SortData.Click += new System.EventHandler(this.SortData_Click_1);
+            // 
+            // ClearData
+            // 
+            this.ClearData.Location = new System.Drawing.Point(12, 238);
+            this.ClearData.Name = "ClearData";
+            this.ClearData.Size = new System.Drawing.Size(75, 23);
+            this.ClearData.TabIndex = 4;
+            this.ClearData.Text = "Clear";
+            this.ClearData.UseVisualStyleBackColor = true;
+            this.ClearData.Click += new System.EventHandler(this.ClearData_Click_1);
+            // 
+            // ListData
+            // 
+            this.ListData.FormattingEnabled = true;
+            this.ListData.Location = new System.Drawing.Point(164, 21);
+            this.ListData.Name = "ListData";
+            this.ListData.Size = new System.Drawing.Size(132, 147);
+            this.ListData.TabIndex = 5;
+            // 
+            // OutPut
+            // 
+            this.OutPut.FormattingEnabled = true;
+            this.OutPut.Location = new System.Drawing.Point(164, 196);
+            this.OutPut.Name = "OutPut";
+            this.OutPut.Size = new System.Drawing.Size(132, 147);
+            this.OutPut.TabIndex = 6;
+            // 
+            // CountAlg
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 382);
+            this.Controls.Add(this.OutPut);
+            this.Controls.Add(this.ListData);
+            this.Controls.Add(this.ClearData);
+            this.Controls.Add(this.SortData);
+            this.Controls.Add(this.DeleteData);
+            this.Controls.Add(this.AddData);
+            this.Controls.Add(this.EnteredData);
+            this.Name = "CountAlg";
+            this.Text = "Count Sort Algorithm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox EnteredData;
+        private System.Windows.Forms.Button AddData;
+        private System.Windows.Forms.Button DeleteData;
+        private System.Windows.Forms.Button SortData;
+        private System.Windows.Forms.Button ClearData;
+        private System.Windows.Forms.ListBox ListData;
+        private System.Windows.Forms.ListBox OutPut;
+    }
+}
+
---
+++ CountSortAlgo/CountSortAlgo/CountAlg.cs
@@ -0,0 +1,116 @@
+﻿// Copyright © 2022 Omar ElSaadany | All rights reserved
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CountSortAlgo
+{
+    public partial class CountAlg : Form
+    {
+        public CountAlg()
+        {
+            InitializeComponent();
+        }
+
+        void ClearOutPut()
+        {
+            OutPut.Items.Clear();
+        }
+
+        void CountSort(int[] arr,int size)
+        {
+            int[] output = new int[size+1];
+            int max = arr[0];
+
+            for(int i=1;i<size;i++)
+            {
+                if (arr[i] > max) 
+                {
+                    max = arr[i]; 
+                }
+            }
+
+            int[] count = new int[max+1];
+
+            for (int i = 0; i < max; ++i)
+            {
+                count[i] = 0;
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                count[arr[i]]++;
+            }
+
+            for (int i = 1; i <= max; i++)
+            {
+                count[i] += count[i - 1];
+            }
+
+            for (int i = size - 1; i >= 0; i--)
+            {
+                output[count[arr[i]] - 1] = arr[i];
+                count[arr[i]]--;
+            }
+
+            for (int i = 0; i < size; ++i)
+            {
+                arr[i] = output[i];
+                OutPut.Items.Add(arr[i]);
+            }
+        }
+
+        private void AddData_Click(object sender, EventArgs e)
+        {
+            EnteredData.Select();
+            int value;
+            if (int.TryParse(EnteredData.Text, out value))
+            { 
+                ListData.Items.Add(value); ClearOutPut();
+            }
+            else
+            { 
+                MessageBox.Show("Enter No. ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            EnteredData.Clear();
+        }
+
+        private void SortData_Click_1(object sender, EventArgs e)
+        {
+            ClearOutPut();
+            int n = ListData.Items.Count;
+            int[] values = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                values[i] = Convert.ToInt32(ListData.Items[i]);
+            }
+            CountSort(values, n);
+        }
+
+        private void DeleteData_Click_1(object sender, EventArgs e)
+        {
+            if (ListData.SelectedItem != null)
+            {
+                ListData.Items.Remove(ListData.SelectedItem);
+                ClearOutPut();
+            }
+            else
+            {
+                MessageBox.Show("Select Element First", "UNSELECTED ELEMENT", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void ClearData_Click_1(object sender, EventArgs e)
+        {
+            ListData.Items.Clear();
+            ClearOutPut();
+        }
+    }
+}
+// Copyright © 2022 Omar ElSaadany | All rights reserved
